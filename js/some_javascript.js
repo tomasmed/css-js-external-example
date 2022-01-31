626e31f779ee4fc6c08e6790eceef5f07b677aa5
Mullins, Benjamin <benm@umich.edu>
2022-01-31 09:28:27 -0500

    first commit

--- js/some_javascript.js
+++ js/some_javascript.js
@@ -1,20 +1,58 @@
-// We wrap all the JS in a function as a way to contain it so it doesn't
-// risk conflicting with other loaded javascript
 (function() {
-    const addStuff = document.querySelector('#javascript-will-add-content');
-    addStuff.textContent = 'This text was added via a dedicated JavaScript file';
- })();
+      // Variables: Assign each button on the page to a variable.
 
 
- // This accomplishes the same thing, but with modern syntax. 
- // This is the recommended syntax, but for those new to JavaScipt, it is 
- // helpful to see the word "function" to describe functions.
- //
- // The differences between function() and () => aren't a concern until 
- // you get into more complex JavaScript
- /*
- (() => {
-    const addStuff = document.querySelector('#javascript-will-add-content');
-    addStuff.textContent = 'This text was added via a dedicated JavaScript file';
+      // Variable: The time display element.
+    
+      // Variable: stores the start time whenever the stopwatch
+      // is started. Declared here so it's available to all functions
+      // to update.
+
+      // Variable: stores the interval id of the once-per-second
+      // timer. Declared here so it can be updated or cleared in any
+      // function.
+
+      // Variable: stores the timer value (how much
+      // time has passed since pressing "start"), so this can 
+      // be increased or reset depending on the function.
+      
+      // Function: updates the contents of timeDisplay with the
+      // current time.
+ 
+
+      // Function: Callback for the setInterval call that fires every 
+      // second when the stopwatch is running. This updates the
+      // timer amount.
+      
+
+      // Function: [Stretch goal] - for enabling/disabling buttons into
+      // a single function instead of each event handler.
+     
+    
+      // Event listener: When the start button is clicked:
+      // - Update the start time variable with the new start time 
+      //   (as a timestamp)
+      // - Start a new interval that 
+      //       -- fires once per second
+      //       -- invokes the callback that updates the timer amount
+      //       -- the interval ID is stored in a variable available to
+      //          other functions.
+      // - Start button disabled
+      // - Stop and reset button enabled
+      
+
+
+      // Event listener: When the stop button is clicked:
+      // - Clear the interval updating the timer value.
+      // - Start and reset button enabled
+      // - Stop button disabled.
+    
+
+      // Event listener: When the reset button is clicked:
+      // - Clear the interval updating the timer value.
+      // - Set the timer amount back to 0
+      // - Call function that updates the timer display
+      // - Start button enabled
+      // - Stop and reset button disabled.
+    
  })();
-*/
