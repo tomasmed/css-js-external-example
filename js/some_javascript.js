2f3b3394495da291ec5a171bff535f2ecba53ece
Barthim <tomasmed@umich.edu>
2022-02-07 17:11:39 -0500

    Leaflet Demo

--- js/some_javascript.js
+++ js/some_javascript.js
@@ -1,58 +1,36 @@
-(function() {
-      // Variables: Assign each button on the page to a variable.
+const map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYmVubXVsbGlucyIsImEiOiJja3phaGJwc20yM3o3MnZyMWNnNnU2dXRiIn0.5xwPYAANdMDPeiyUccIooQ', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiYmVubXVsbGlucyIsImEiOiJja3phaGJwc20yM3o3MnZyMWNnNnU2dXRiIn0.5xwPYAANdMDPeiyUccIooQ'
+}).addTo(map);
+var marker = L.marker([51.5, -0.09]).addTo(map);
+var circle = L.circle([51.508, -0.11], {
+      color: 'red',
+      fillColor: '#f03',
+      fillOpacity: 0.5,
+      radius: 500
+  }).addTo(map);
+  var polygon = L.polygon([
+      [51.509, -0.08],
+      [51.503, -0.06],
+      [51.51, -0.047]
+  ]).addTo(map);
 
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
 
-      // Variable: The time display element.
-    
-      // Variable: stores the start time whenever the stopwatch
-      // is started. Declared here so it's available to all functions
-      // to update.
+var popup = L.popup();
 
-      // Variable: stores the interval id of the once-per-second
-      // timer. Declared here so it can be updated or cleared in any
-      // function.
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
 
-      // Variable: stores the timer value (how much
-      // time has passed since pressing "start"), so this can 
-      // be increased or reset depending on the function.
-      
-      // Function: updates the contents of timeDisplay with the
-      // current time.
- 
-
-      // Function: Callback for the setInterval call that fires every 
-      // second when the stopwatch is running. This updates the
-      // timer amount.
-      
-
-      // Function: [Stretch goal] - for enabling/disabling buttons into
-      // a single function instead of each event handler.
-     
-    
-      // Event listener: When the start button is clicked:
-      // - Update the start time variable with the new start time 
-      //   (as a timestamp)
-      // - Start a new interval that 
-      //       -- fires once per second
-      //       -- invokes the callback that updates the timer amount
-      //       -- the interval ID is stored in a variable available to
-      //          other functions.
-      // - Start button disabled
-      // - Stop and reset button enabled
-      
-
-
-      // Event listener: When the stop button is clicked:
-      // - Clear the interval updating the timer value.
-      // - Start and reset button enabled
-      // - Stop button disabled.
-    
-
-      // Event listener: When the reset button is clicked:
-      // - Clear the interval updating the timer value.
-      // - Set the timer amount back to 0
-      // - Call function that updates the timer display
-      // - Start button enabled
-      // - Stop and reset button disabled.
-    
- })();
+map.on('click', onMapClick);
